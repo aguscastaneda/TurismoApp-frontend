d608ec2608dee1007d8b4aa9bc3da96c5777f308
aguscastaneda <castaneda.agustin.et21.21@gmail.com>
2025-06-28 22:54:58 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-  // https://vitejs.dev/config/
-  export default defineConfig({
-    plugins: [react()],
-    server: {
-      port: 5173,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3000',
-          changeOrigin: true,    
-        },
-      },
-      host: true
-    }
-  }) 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // 👈 Asegura que los recursos carguen correctamente
+})
